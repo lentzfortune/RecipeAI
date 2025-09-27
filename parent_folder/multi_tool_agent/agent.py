2581cda9afdc5b9fb872d76bea86f5793e3c8183
Lentz Fortune <117185451+lentzfortune@users.noreply.github.com>
2025-09-27 11:44:29 -0400

    Add files via upload

---
+++ parent_folder/multi_tool_agent/agent.py
@@ -0,0 +1,74 @@
+import datetime
+from zoneinfo import ZoneInfo
+from google.adk.agents import Agent
+
+def greetUser():
+    print("Hello What can I help you get ready today?")
+
+
+
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the weather report.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+    if city.lower() == "new york":
+        return {
+            "status": "success",
+            "report": (
+                "The weather in New York is sunny with a temperature of 25 degrees"
+                " Celsius (77 degrees Fahrenheit)."
+            ),
+        }
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Weather information for '{city}' is not available.",
+        }
+
+
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the current time.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+
+    if city.lower() == "new york":
+        tz_identifier = "America/New_York"
+    else:
+        return {
+            "status": "error",
+            "error_message": (
+                f"Sorry, I don't have timezone information for {city}."
+            ),
+        }
+
+    tz = ZoneInfo(tz_identifier)
+    now = datetime.datetime.now(tz)
+    report = (
+        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    )
+    return {"status": "success", "report": report}
+
+
+root_agent = Agent(
+    name="Recipe Agent",
+    model="gemini-2.0-flash",
+    description=(
+        "Agent that suggest recipes for user after provides them with a list of ingredients they have"
+    ),
+    instruction=(
+        "You are a helpful agent that can suggest recipes to users when they provide with ingredients.",
+        "The ingredients can be given to you in picture format or listed"
+        "When they give you the ingredients in picture format, you ask them if you missed anything"
+    ),
+    tools=[get_weather, get_current_time, greetUser],
+)
